e52f15c6ae219f6833d3054f50e11fc0c481ef4f
cluelessmommy <runa.hosne@gmail.com>
2026-09-05 07:36:44 +0000

    server dictionary moved to inside of a function

--- server_monitor.py
+++ server_monitor.py
@@ -2,18 +2,26 @@ import psutil
 import time
 from datetime import datetime
 
+THRESHOLD = 80
+
 def get_server_info():
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    return cpu, memory, disk
+
+    server = {
+        "cpu" : cpu,
+        "memory" : memory,
+        "disk" : disk
+    }
+    return server
 
 def get_server_health_status(server):
 
 
-    print(f"CPU: {cpu}")
-    print(f"MEMORY: {memory}")
-    print(f"DISK: {disk}")
+    print(f"CPU: {server['cpu']}")
+    print(f"MEMORY: {server['memory']}")
+    print(f"DISK: {server['disk']}")
 
 
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -23,8 +31,8 @@ def get_server_health_status(server):
     print("       Time:", current_time)
     print("================================")
 
-    THRESHOLD = 80
-    if cpu < THRESHOLD and memory < THRESHOLD and disk < THRESHOLD:
+
+    if server['cpu'] < THRESHOLD and server['memory'] < THRESHOLD and server['disk'] < THRESHOLD:
        # print("SERVER HEALTH STATUS: HEALTHY")
         return "HEALTHY"
     else:
@@ -32,12 +40,8 @@ def get_server_health_status(server):
         return "CRITICAL"   
 
 for i in range(3):
-    cpu, memory, disk = get_server_info()
-    server = {
-        "cpu" : cpu,
-        "memory" : memory,
-        "disk" : disk
-    }
+    server = get_server_info()
+
 
     status = get_server_health_status(server)
 
