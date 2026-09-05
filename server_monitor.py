4bfb77e6ced6a20dbeb7283e1dff20599a67b56e
cluelessmommy <runa.hosne@gmail.com>
2026-09-05 03:52:14 +0000

    Refractor server monitor with functions

--- server_monitor.py
+++ server_monitor.py
@@ -2,22 +2,23 @@ import psutil
 import time
 from datetime import datetime
 
-
-
-for i in range(3):
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print("Time:", current_time)
-    print("================================")
-    print("       SERVER HEALTH CHECK")
-    print("================================")
+def get_server_info():
     cpu = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    cpu_count = psutil.cpu_count(logical=True)
+    return cpu, memory, disk
+
+def get_server_health_status(cpu, memory, disk):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    print("================================")
+    print("       SERVER HEALTH CHECK")
+    print("       Time:", current_time)
+    print("================================")
     print(f"CPU Usage: {cpu}%")
     print(f"Memory Usage: {memory}%")
     print(f"Disk Usage: {disk}%")
-    print(f"Number of CPU: {cpu_count}")
+    #print(f"Number of CPU: {cpu_count}")
     #health status check
     print("================================")
     print("       SERVER HEALTH STATUS")
@@ -38,5 +39,13 @@ for i in range(3):
     else:
       print("Disk need attention") 
     
+    
+
+for i in range(3):
+    cpu, memory, disk = get_server_info()
+    get_server_health_status(cpu, memory, disk)
     time.sleep(5)
 
+
+
+    
